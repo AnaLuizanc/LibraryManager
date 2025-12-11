97b22c743f695a5c9f4665ed51538455e1e6e7c9
AnaLuizanc <analuizanc13@gmail.com>
2025-12-11 08:10:45 -0300

    feat: add Author and Book entity classes with JPA annotations

---
+++ src/main/java/com/springbootRest/springbootRest/entity/Author.java
@@ -0,0 +1,19 @@
+package com.springbootRest.springbootRest.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            nullable = false,
+            unique = true
+    )
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
