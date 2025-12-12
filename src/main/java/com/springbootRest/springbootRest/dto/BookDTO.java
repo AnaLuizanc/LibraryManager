e6e7521e114bb92d55e142e30666df25fe756da7
Ana Luiza Nobre Cordeiro <ana.nobre@montesclaros.mg.gov.br>
2025-12-12 10:06:10 -0300

    feat: add AuthorRepository, update BookService to check for existing authors by name

--- src/main/java/com/springbootRest/springbootRest/dto/BookDTO.java
+++ src/main/java/com/springbootRest/springbootRest/dto/BookDTO.java
@@ -30,7 +30,7 @@ public class BookDTO {
     private Integer chapters;
 
     @NotNull(message = "ISBN não pode ser nulo")
-    @Pattern(regexp = "^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$",
+    @Pattern(regexp = "^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\\d-]+$",
             message = "ISBN deve estar em formato válido")
     private String isbn;
 
--- src/main/java/com/springbootRest/springbootRest/service/BookService.java
+++ src/main/java/com/springbootRest/springbootRest/service/BookService.java
@@ -2,28 +2,40 @@ package com.springbootRest.springbootRest.service;
 
 import com.springbootRest.springbootRest.dto.BookDTO;
 import com.springbootRest.springbootRest.dto.MessageResponseDTO;
+import com.springbootRest.springbootRest.entity.Author;
 import com.springbootRest.springbootRest.entity.Book;
 import com.springbootRest.springbootRest.mapper.BookMapper;
+import com.springbootRest.springbootRest.repository.AuthorRepository;
 import com.springbootRest.springbootRest.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class BookService {
     private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
 
     private final BookMapper bookMapper = BookMapper.INSTANCE;
 
     @Autowired
-    public BookService(BookRepository bookRepository) {
+    public BookService(BookRepository bookRepository, AuthorRepository authorRepository) {
         this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
     }
 
     public MessageResponseDTO create(BookDTO bookDTO) {
         Book bookToSave = bookMapper.toModel(bookDTO);
+
+        // Verificar se o autor já existe pelo nome
+        Optional<Author> existingAuthor = authorRepository.findByName(bookDTO.getAuthor().getName());
+        if (existingAuthor.isPresent()) {
+            bookToSave.setAuthor(existingAuthor.get());
+        }
+
         Book savedBook = bookRepository.save(bookToSave);
         return MessageResponseDTO.builder().message("Book created with ID " + savedBook.getId()).build();
     }
