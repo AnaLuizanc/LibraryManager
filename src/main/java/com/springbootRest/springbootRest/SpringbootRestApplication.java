3787056b351d0a429de7d17a950e42596f8542de
Ana Luiza Nobre Cordeiro <ana.nobre@montesclaros.mg.gov.br>
2025-12-15 09:57:21 -0300

    feat: add home endpoint to SpringbootRestApplication for a simple greeting

--- src/main/java/com/springbootRest/springbootRest/SpringbootRestApplication.java
+++ src/main/java/com/springbootRest/springbootRest/SpringbootRestApplication.java
@@ -2,12 +2,19 @@ package com.springbootRest.springbootRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestApplication.class, args);
 	}
 
+    @GetMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
 }
